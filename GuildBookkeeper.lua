ac4aadf60e9b1bec59e280ef223d6937de60dd1e
unusualpi <UnusualPi@gmail.com>
2022-07-31 12:37:01 -0600

    version update

--- GuildBookkeeper.lua
+++ GuildBookkeeper.lua
@@ -24,7 +24,7 @@ local LAM = LibAddonMenu2
 
 local GBK = {}
 GBK.name = "GuildBookkeeper"
-GBK.version = "0.0.5"
+GBK.version = "0.0.6"
 GBK.author = 'ArcHouse'
 GBK.saveVars = 'GuildBookkeeperVariables'
 GBK.default = {guilds={}, ledger={}}
